21900dbeac93a63beee349dab862ca50e08267a8
alvin-c-cruz <alvinccruz12@gmail.com>
2023-12-16 23:25:54 +0800

    Updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='acas_auth',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     install_requires=[
         "Flask==3.0.0",
